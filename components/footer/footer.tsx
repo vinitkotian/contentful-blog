a5fc996819e9c109590ab985951b57a85fa8c844
vinitkotian <kotianvinit@gmail.com>
2022-05-09 11:15:43 +0530

    Add footer content

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,35 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Box, flexbox } from "@mui/system";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer: React.FC<{}> = (props) => {
   return (
     <AppBar position="static" className={"footer"}>
-      <Toolbar></Toolbar>
+      <Toolbar>
+        <Box className={"footer-col footer-col1"}>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>Terms And Conditions</a>
+          </Link>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>Privacy Policy</a>
+          </Link>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>Cookie Policy</a>
+          </Link>
+        </Box>
+        <Box className={"footer-col"}>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>About Us</a>
+          </Link>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>Contact Us</a>
+          </Link>
+          <Link href="/">
+            <a style={{ margin: "0.2rem" }}>Kickstarter to Blogspace</a>
+          </Link>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 };
