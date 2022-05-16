fd30e55bf2c9eeb7ddcbf3eebd87e77d5d402a29
vinitkotian <kotianvinit@gmail.com>
2022-05-16 09:37:49 +0530

    Fix animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,48 +21,6 @@ const useStyles = makeStyles({
     color: "white",
     margin: "5rem 5rem 0 0",
     textTransform: "capitalize",
-  },
-  browseLinkBtn: {
-    width: "20rem",
-    height: "2.8rem",
-    marginRight: "5rem",
-    borderRadius: "0.8rem",
-    border: "0.1rem solid white",
-    color: "white",
-    backgroundColor: "transparent",
-    font: 'bold 1.2rem "Anton", sans-serif',
-    cursor: "pointer",
-    '&:hover':{
-      animationName: "btn-colours",
-      animationDuration: "5s",
-      animationIterationCount: "infinite"
-    }
-  },
-  "@keyframes btn-colours": {
-    "0%": {
-      border: "0.1rem solid #4716F8",
-      color: "#4716F8"
-    },
-    "20%": {
-      border: "0.1rem solid rgb(228, 74, 74)",
-      color: "rgb(228, 74, 74)"
-    },
-    "40%": {
-      border: "0.1rem solid rgb(160, 28, 153)",
-      color: "rgb(160, 28, 153)"
-    },
-    "60%": {
-      border: "0.1rem solid rgb(201, 204, 17)",
-      color: "rgb(201, 204, 17)"
-    },
-    "80%": {
-      border: "0.1rem solid rgb(23, 204, 17)",
-      color: "rgb(23, 204, 17)"
-    },
-    "100%": {
-      border: "0.1rem solid rgb(77, 156, 221)",
-      color: "rgb(77, 156, 221)"
-    }
   }
 });
 
@@ -73,7 +31,7 @@ const HomePage: React.FC<{}> = () => {
       <Box className={classes.backdrop}>
         <div className={classes.welcomeText}>Welcome to Blogspace.</div>
         <Link href={"/browse"} passHref>
-          <button className={classes.browseLinkBtn}>Explore</button>
+          <button className={"browse-link-btn"}>Explore</button>
         </Link>
       </Box>
     </Box>
